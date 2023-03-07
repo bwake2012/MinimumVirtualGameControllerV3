02f7453927ed2442d8e4febed1585b1b1d1144cc
Bob Wakefield <bob@cockleburr.net>
2023-03-06 21:48:35 -0600

    Initial Commit

---
+++ MinimumVirtualGameControllerV3/ViewController.swift
@@ -0,0 +1,23 @@
+// ViewController.swift
+//
+// Created by Bob Wakefield on 3/6/23.
+// for MinimumVirtualGameControllerV3
+//
+// Using Swift 5.0
+// Running on macOS 13.2
+//
+// 
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
